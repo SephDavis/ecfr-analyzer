2d2cac72ea539b4d31d42335407455317b5820c8
Toby Davis <toby.daviss@gmail.com>
2025-04-09 20:40:19 -0500

    Build frontend and update backend to serve static React app

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -9,7 +9,7 @@ app.get('/agencies', async (req, res) => {
     try {
       const response = await axios.get('https://www.ecfr.gov/api/admin/v1/agencies.json');
       res.setHeader('Access-Control-Allow-Origin', '*'); // ✅ Force CORS for Railway
-      res.json(response.data);
+      res.json({ agencies: response.data });
     } catch (err) {
       console.error(err.message);
       res.status(500).json({ error: 'Failed to fetch agencies data' });
